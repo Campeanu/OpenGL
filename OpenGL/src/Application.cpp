83650743320864d0ebbd77c5135b234637f209b4
Campeanu <campeanu.it@yahoo.com>
2019-07-14 14:40:41 +0300

    Add ImGUI

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <stdio.h>
 
 #include "Renderer.hpp"
 #include "VertexBuffer.hpp"
@@ -22,6 +23,11 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -32,9 +38,9 @@ int main(void)
 	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 	//glfwWindowHint(GLFW_OPENGL_ANY_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+	//glfwWindowHint(GLFW_DECORATED, false);
 	/* Create a windowed mode window and its OpenGL context */
-	window =  glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window =  glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
 	
 	if (!window)
 	{
@@ -50,10 +56,10 @@ int main(void)
 
 	{
 		float position[] = {
-			-0.5f, -0.5f, 0.0f, 0.0f, // 0
-			 0.5f, -0.5f, 1.0f, 0.0f, // 1
-			 0.5f,  0.5f, 1.0f, 1.0f, // 2
-			-0.5f,  0.5f, 0.0f, 1.0f  // 3
+			100.0f, 100.0f, 0.0f, 0.0f, // 0
+			200.0f, 100.0f, 1.0f, 0.0f, // 1
+			200.0f, 200.0f, 1.0f, 1.0f, // 2
+			100.0f, 200.0f, 0.0f, 1.0f  // 3
 		};
 
 		unsigned int indices[] = {
@@ -79,9 +85,12 @@ int main(void)
 
 		IndexBuffer ib(indices, 6);
 
-		glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
 
-		Shader shader("res/shaders/Basic.shader");
+		glm::vec4 result = proj * vp;
+	
+		Shader shader("res/shaders/basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
 		shader.SetUniformMat4f("u_MVP", proj);
@@ -98,9 +107,24 @@ int main(void)
 
 		Renderer renderer;
 
+		ImGui::CreateContext();
+		// Setup Dear ImGui style
+		ImGui::StyleColorsDark();
+    	//ImGui::StyleColorsClassic();
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		//ImGui_ImplOpenGL3_Init(glsl_version);
+
+		bool show_demo_window = true;
+		bool show_another_window = false;
+		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+		
 		while (!glfwWindowShouldClose(window))
 		{
 			renderer.Clear();
+
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
 
 			shader.Bind();
 			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
@@ -121,10 +145,38 @@ int main(void)
 
 			r += increment;
 
+			{
+				static float f = 0.0f;
+				static int counter = 0;
+
+				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+				ImGui::Checkbox("Another Window", &show_another_window);
+
+				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+					counter++;
+				ImGui::SameLine();
+				ImGui::Text("counter = %d", counter);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				ImGui::End();
+			}
+
+			ImGui::Render();
+        	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+			
 			glfwSwapBuffers(window);
 			glfwPollEvents();
 		}
 	}
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
 	glfwTerminate(); 
 }
